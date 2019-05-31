ba88420b32fd761ec1590f360e561f935914c535
JulioAndresSalazar <48372550+JulioAndresSalazar@users.noreply.github.com>
2019-05-31 13:10:56 -0400

    Update CloudCode.py

--- CloudCode.py
+++ CloudCode.py
@@ -1,26 +1,25 @@
+#Import required libraries
 import urequests, ujson
 
-#DO NOT CHANGE
-#API Info needed for both Get and Put
+#NI Systenm Link Cloud API Info setup
 Key = "sfIbD4eYzaMBqna_rZW5XQL2BR77CLb2I4BDvI6uxt" #global
 urlBase = "https://api.systemlinkcloud.com/nitag/v2/tags/" #global
-headers = {"Accept":"application/json","x-ni-api-key":Key} #global
-#"Content-Type":"application/json"
+headers = {"Content-Type":"application/json", "Accept":"application/json","x-ni-api-key":Key} #global
 
-#Gets tage value
 def cloudGet(tag_name):
+    #This function gets the tag value from the cloud
     urlValue = urlBase + tag_name + "/values/current"
     value = urequests.get(urlValue,headers=headers).text
     data = ujson.loads(value)
     result = data.get("value").get("value")
     return result
 
-#Updates tag in cloud
 def cloudPut(tag, tag_type, val):
+    #This function updates the tag value in the cloud
     Value = val
     Type = tag_type
     propValue = {"value":{"type":Type,"value":Value}}
     urlValue = urlBase + tag + "/values/current"
     print(urequests.put(urlValue,headers=headers,json=propValue).text)
     
-cloudPut("LEDPuzzle", "STRING", "JulioTest")
+cloudGet("LEDPuzzle")
