996fd1eef4f57a055efbf2186e9d1df253a42cc2
JulioAndresSalazar <48372550+JulioAndresSalazar@users.noreply.github.com>
2019-05-31 12:04:20 -0400

    Update ClueBox(LED).py

--- ClueBox(LED).py
+++ ClueBox(LED).py
@@ -1,13 +1,13 @@
+#Import necessary libraries 
 import ujson, urequests, utime, machine, urandom
-#works on esp32
 
-#DO NOT CHANGE
 #API Info
 Key = "sfIbD4eYzaMBqna_rZW5XQL2BR77CLb2I4BDvI6uxt" #global
 urlBase = "https://api.systemlinkcloud.com/nitag/v2/tags/" #global
 headers = {"Content-Type":"application/json", "Accept":"application/json","x-ni-api-key":Key} #global
 
 def cloudGet(tag_name):
+    #This function returns the tag value from the cloud
     urlValue = urlBase + tag_name + "/values/current"
     value = urequests.get(urlValue,headers=headers).text
     data = ujson.loads(value)
@@ -15,17 +15,20 @@ def cloudGet(tag_name):
     return result
 
 def cloudPut(tag, tag_type, val):
+    #This function updates the tag value in the cloud
     Value = val
     Type = tag_type
     propValue = {"value":{"type":Type,"value":Value}}
     urlValue = urlBase + tag + "/values/current"
     print(urequests.put(urlValue,headers=headers,json=propValue).text)
-    #requests.put(urlValue,headers=headers,json=propValue).text 
-
+    
+#Pin setup    
 lock = machine.Pin(23, machine.Pin.OUT)
 lock.value(0)
-start = utime.time()
 
+#Start of main loop
+#The processor constantly  reads the tag value. When the puzzle is unsolved it keeps the box closed. When the puzzle is solved
+#the solenoid engages and unlocks the box
 while True:
     status = cloudGet("LEDPuzzle")
     end = utime.time()
@@ -33,13 +36,11 @@ while True:
     print('Status: ' + str(status))
     print('Minutes since start: ' + str((end - start)/60.0) +'\n')
     if status == "SOLVED" and lock.value() == 0:
-        #print('solved')
+        print('solved')
         lock.on()
     elif status == "UNSOLVED" and lock.value() == 1:
-        #print('waiting...')
+        print('waiting...')
         lock.off()
     else:
         utime.sleep(0.5)
     utime.sleep(0.5)    
-
-print('Puzzle has been solved')    
